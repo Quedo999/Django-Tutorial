20177585498754516dc438dac3fbe717d5934b09
Quedo999 <sri00119@naver.com>
2022-11-22 23:52:52 +0900

    # django tutorial commit

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,32 @@
+import datetime
 from django.db import models
+from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
+class Question(models.Model):
+    def __str__(self):
+        return self.question_text
+
+
+    @admin.display(
+            boolean=True,
+            ordering='pub_date',
+            description='Published recently?',
+        )
+    def was_published_recently(self):
+        # 미래의 데이터가 들어갈 경우 False를 반환해야함. 에러코드가 된다.
+        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,124 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Choice, Question
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+# 함수형 기반 뷰
+# def index(request):
+#     # 1
+#     # return HttpResponse("Hello, world. You're at the polls index.")
+
+#     # 2
+#     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # output = ', '.join([q.question_text for q in latest_question_list])
+#     # return HttpResponse(output)
+
+#     # 3 템플릿 사용
+#     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # template = loader.get_template('polls/index.html')
+#     # context = {
+#     #     'latest_question_list': latest_question_list,
+#     # }
+#     # return HttpResponse(template.render(context, request))
+
+#     # 4 shortcut - render 사용
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return render(request, 'polls/index.html', context)
+
+# def detail(request, question_id):
+#     # 1
+#     # return HttpResponse("You're looking at question %s." % question_id)
+    
+#     # 2 404 error 설정
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     # return render(request, 'polls/detail.html', {'question': question})
+
+#     # 3
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+# def results(request, question_id):
+#     # 1
+#     # respons = "You're looking at the results of question %s."
+#     # return HttpResponse(respons % question_id)
+
+#     # 2
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question':question})
+
+# def vote(request, question_id):
+#     # 1
+#     # return HttpResponse("You're voting on question %s." % question_id)
+
+#     # 2
+#     question = get_object_or_404(Question, pk=question_id)
+#     try:
+#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+#     except (KeyError, Choice.DoesNotExist):
+#         return render(request, 'polls/detail.html', {
+#             'question': question,
+#             'error_message': "You didn't select a choice.",
+#         })
+#     else:
+#         selected_choice.votes += 1
+#         selected_choice.save()
+#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+# 제네릭기반 뷰
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    # def get_queryset(self):
+    #     return Question.objects.order_by('-pub_date')[:5]
+
+    def get_queryset(self):
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+class DetailView(generic.DeleteView):
+    model = Question
+    template_name: str = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name: str = 'polls/results.html'
+
+
+def vote(request, question_id):
+    # 1
+    # return HttpResponse("You're voting on question %s." % question_id)
+
+    # 2
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
